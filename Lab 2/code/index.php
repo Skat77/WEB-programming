1901935993c71a06fc7d67581a62a674d07bece9
Skat77 <159482597+Skat77@users.noreply.github.com>
2024-03-09 20:33:19 +0200

    Added Task 2

--- Lab 2/code/index.php
+++ Lab 2/code/index.php
@@ -10,3 +10,20 @@ $order .= " and 5 nuggets, please";
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
 
+echo "<br><br> Task 2 <br>";
+
+$var1 = 17;
+echo $var1;
+echo "<br>";
+$var2 = 5;
+echo $var2;
+echo "<br>";
+$var3 = 10.3;
+echo $var3;
+echo "<br>";
+echo $var1 - $var2;
+echo "<br>";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
+echo "<br>";
