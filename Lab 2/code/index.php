4bdc443be1d2f038a0c8fabbc15305456c438136
Skat77 <159482597+Skat77@users.noreply.github.com>
2024-03-09 20:41:26 +0200

    Added Task 11

--- Lab 2/code/index.php
+++ Lab 2/code/index.php
@@ -27,3 +27,12 @@ $last_month = 1187.23;
 $this_month = 1089.98;
 echo $last_month - $this_month;
 echo "<br>";
+
+echo "<br> Task 11 <br>";
+
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo $days_per_language;
+echo "<br>";
