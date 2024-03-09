568023008245cef0d974d57cc9483c9789987cbb
Skat77 <159482597+Skat77@users.noreply.github.com>
2024-03-09 20:10:49 +0200

    Added Task 1

--- Lab 2/code/index.php
+++ Lab 2/code/index.php
@@ -1,2 +1,12 @@
 <?php
-echo phpinfo();
+echo "Task 1<br>";
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = &$very_bad_unclear_name;
+$order .= " and 5 nuggets, please";
+
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
